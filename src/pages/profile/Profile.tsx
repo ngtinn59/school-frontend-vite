c238be375e911e82370ca820f55cc960ec7b4909
Tang Thien Dat <thiendattang@gmail.com>
2024-11-04 20:03:56 +0700

    ux: add profile loading

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import Wrapper from "../../components/Wrapper";
 // import { ProfileDataForCV } from "../../utils/type";
 import { useSelector } from "react-redux";
-import { getUserProfile } from "../../services/redux/user";
+import { getUserInformation, getUserProfile } from "../../services/redux/user";
 import AboutMe from "./components/AboutMe";
 import Award from "./components/Award";
 import Certificate from "./components/Certificate";
@@ -17,15 +17,19 @@ import WorkExperience from "./components/WorkExperience";
 export default function Profile() {
   const profileForCV = useSelector(getUserProfile);
   const [profileCTA, setProfileCTA] = useState<string[]>([]);
+  const userData = useSelector(getUserInformation);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (userData.name) setLoading(false);
+  }, [userData]);
 
   // Check user quality
   useEffect(() => {
     const tempCTA = [];
-    if (profileForCV.aboutMe?.description?.length === 0)
-      tempCTA.push("Add About Me");
+    if (profileForCV.aboutMe?.description?.length === 0) tempCTA.push("Add About Me");
     if (profileForCV.education?.length === 0) tempCTA.push("Add Education");
-    if (profileForCV.workExperience?.length === 0)
-      tempCTA.push("Add Work Experience");
+    if (profileForCV.workExperience?.length === 0) tempCTA.push("Add Work Experience");
 
     if (
       !(
@@ -35,31 +39,29 @@ export default function Profile() {
       )
     )
       tempCTA.push("Add Skills");
-    if (profileForCV.personalProjects?.length === 0)
-      tempCTA.push("Add Personal Projects");
-    if (profileForCV.certificates?.length === 0)
-      tempCTA.push("Add Certificates");
+    if (profileForCV.personalProjects?.length === 0) tempCTA.push("Add Personal Projects");
+    if (profileForCV.certificates?.length === 0) tempCTA.push("Add Certificates");
     if (profileForCV.awards?.length === 0) tempCTA.push("Add Awards");
 
     setProfileCTA(tempCTA);
   }, [profileForCV]);
   return (
-    <Wrapper className="">
-      <div className="grid grid-cols-10 gap-6">
-        <ProfileQuality profileCTA={profileCTA} />
-        <div className="md:col-span-7 col-span-10 gap-6 grid">
-          <UserInformation />
-          <AboutMe aboutMe={profileForCV.aboutMe} />
-          <Education educationList={profileForCV.education} />
-          <WorkExperience workExperienceList={profileForCV.workExperience} />
-          <Skill skills={profileForCV.skills} />
-          <PersonalProject
-            personalProjectList={profileForCV.personalProjects}
-          />
-          <Certificate certificateList={profileForCV.certificates} />
-          <Award awardList={profileForCV.awards} />
+    !loading && (
+      <Wrapper className="">
+        <div className="grid grid-cols-10 gap-6">
+          <ProfileQuality profileCTA={profileCTA} />
+          <div className="md:col-span-7 col-span-10 gap-6 grid">
+            <UserInformation />
+            <AboutMe aboutMe={profileForCV.aboutMe} />
+            <Education educationList={profileForCV.education} />
+            <WorkExperience workExperienceList={profileForCV.workExperience} />
+            <Skill skills={profileForCV.skills} />
+            <PersonalProject personalProjectList={profileForCV.personalProjects} />
+            <Certificate certificateList={profileForCV.certificates} />
+            <Award awardList={profileForCV.awards} />
+          </div>
         </div>
-      </div>
-    </Wrapper>
+      </Wrapper>
+    )
   );
 }
