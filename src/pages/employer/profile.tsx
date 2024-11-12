95036609a680265e4aeb5b87d5c63ee8363266df
Tang Thien Dat <thiendattang@gmail.com>
2024-11-12 22:05:11 +0700

    feat: add type and api

--- src/pages/employer/profile.tsx
+++ src/pages/employer/profile.tsx
@@ -41,13 +41,8 @@ export const EmployerProfile = () => {
 
     return Object.values(displayData).map((info) => {
       return (
-        <div
-          key={info.label}
-          className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0"
-        >
-          <dt className="text-sm font-medium leading-6 text-gray-900">
-            {info.label}
-          </dt>
+        <div key={info.label} className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+          <dt className="text-sm font-medium leading-6 text-gray-900">{info.label}</dt>
           <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
             {info.value}
           </dd>
@@ -75,12 +70,8 @@ export const EmployerProfile = () => {
         />
       </div>
       <div className="px-4 sm:px-0">
-        <h3 className="text-base font-semibold leading-7 text-gray-900">
-          Employer Profile
-        </h3>
-        <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">
-          details and information.
-        </p>
+        <h3 className="text-base font-semibold leading-7 text-gray-900">Employer Profile</h3>
+        <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">details and information.</p>
       </div>
       <div className="mt-6 border-t border-gray-100">
         <dl className="divide-y divide-gray-100">{renderProfile()}</dl>
