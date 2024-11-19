b1157f4d07f242db992508b112c06db44565beac
namtang83 <143093635+namtang83@users.noreply.github.com>
2024-11-20 00:02:35 +0700

    feat: add change pass user

--- src/ui/Layout/Header.tsx
+++ src/ui/Layout/Header.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, redirect } from "react-router-dom";
 import logo from "../../assets/brand_logo.svg";
@@ -11,8 +11,11 @@ import Button from "../../components/Button";
 import { signOutApi } from "../../services/api/authenticationApi";
 import deleteCookie from "../../utils/function/deleteCookie";
 import toast from "react-hot-toast";
+import ModalChangePasswordUser from "../../pages/profile/ModalChangePasswordUser";
+import ModalChangePassword from "../../pages/employer/Modal/ModalChangePassword";
 
 const Header: React.FC<{ moreMenu?: React.JSX.Element }> = ({ moreMenu }) => {
+  const [openModal, setOpenModal] = useState(false);
   const user = useSelector(getUserAuthentication);
   const userInformation = useSelector(getUserInformation);
   const dispatch = useDispatch();
@@ -31,14 +34,14 @@ const Header: React.FC<{ moreMenu?: React.JSX.Element }> = ({ moreMenu }) => {
     dispatch(signOut());
   };
   return (
-    <header className="bg-gray-800 	max-w-full fixed right-0 left-0 z-50 top-0 h-fit">
-      <div className="flex items-center justify-between px-4 py-2 mx-auto xl:max-w-[1280px]">
+    <header className="fixed left-0 right-0 top-0 z-50 h-fit max-w-full bg-gray-800">
+      <div className="mx-auto flex items-center justify-between px-4 py-2 xl:max-w-[1280px]">
         <Link to="/" className="text-white">
           <img src={logo} alt="brand-logo" width={60} />
         </Link>
 
         <nav>
-          <ul className="flex space-x-4 items-center">
+          <ul className="flex items-center space-x-4">
             {/* <li>
               <Link to="/" className="text-white">
                 Tìm kiếm công việc
@@ -71,24 +74,32 @@ const Header: React.FC<{ moreMenu?: React.JSX.Element }> = ({ moreMenu }) => {
                 <span className="group relative">
                   <Link
                     to="/profile"
-                    className="text-white flex flex-row items-center flex-1 gap-2 group/item "
+                    className="group/item flex flex-1 flex-row items-center gap-2 text-white"
                   >
                     Chào mừng {user.name}
                     <div
-                      className="bg-cover bg-center bg-no-repeat rounded-full relative block p-1 aspect-square w-8 shadow-md"
+                      className="relative block aspect-square w-8 rounded-full bg-cover bg-center bg-no-repeat p-1 shadow-md"
                       style={{
                         backgroundImage: `url(${userInformation.image_url})`,
                       }}
                     ></div>
                   </Link>
-                  <Button
-                    className="p-2 rounded-md !hidden group-hover:!block absolute w-full !bg-red-600"
-                    // backgroundColor="#fff"
-                    // textColor="#000"
-                    onClick={handleSignOut}
-                  >
-                    Sign out
-                  </Button>
+                  <div className="absolute hidden w-full flex-col items-center gap-2 group-hover:flex">
+                    <Button
+                      className="w-full rounded-md !bg-red-600 p-2"
+                      onClick={() => {
+                        setOpenModal(true);
+                      }}
+                    >
+                      Change password
+                    </Button>
+                    <Button
+                      className="w-full rounded-md !bg-red-600 p-2"
+                      onClick={handleSignOut}
+                    >
+                      Sign out
+                    </Button>
+                  </div>
                 </span>
               ) : (
                 <div className="flex gap-3">
@@ -104,6 +115,7 @@ const Header: React.FC<{ moreMenu?: React.JSX.Element }> = ({ moreMenu }) => {
           </ul>
         </nav>
       </div>
+      <ModalChangePassword open={openModal} setOpen={setOpenModal} />
     </header>
   );
 };
